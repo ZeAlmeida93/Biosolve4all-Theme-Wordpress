a048bdfdbc4307198b16d09abe5e581e689af63b
Jose Almeida <52549510+ZeAlmeida93@users.noreply.github.com>
2026-01-10 13:11:59 +0000

    update popuo form and redirects

--- page-en.php
+++ page-en.php
@@ -1,6 +1,8 @@
 <?php
 /* English Page Template (page slug: en) */
 get_header();
+$lang = function_exists( 'biosolve4all_get_lang' ) ? biosolve4all_get_lang() : 'en';
+$news_url = $lang === 'en' ? home_url( '/en/noticias/' ) : home_url( '/noticias/' );
 ?>
 
 <section class="container" id="hero-section">
@@ -257,7 +259,7 @@ get_header();
 <div id="lottie-news"></div>
 <h3>News</h3>
 <p>Stay up to date with the latest news</p>
-<a href="https://biosolve4all.com/noticias/">
+<a href="<?php echo esc_url( $news_url ); ?>">
 <button>View News</button></a>
 </div>
 </div>
@@ -303,178 +305,13 @@ get_header();
 <li><a href="#processo">Process</a></li>
 <li><a href="#equipa">Team</a></li>
 <li><a href="#parceiros">Partners &amp; Clients</a></li>
-<li><a href="https://biosolve4all.com/noticias/">News</a></li>
+<li><a href="<?php echo esc_url( $news_url ); ?>">News</a></li>
 <li><a href="#contactos">Contact</a></li>
 <div>
 <a id="mobile-newsletter-menu-overlay">Newsletter</a>
 </div>
 </ul>
 </div>
-</div><div id="newsletter-pop-up">
-<!-- Begin Brevo Form -->
-<!-- START - We recommend to place the below code in head tag of your website html  -->
-<style>
-    @font-face {
-      font-display: block;
-      font-family: Figtree;
-      src: url(https://assets.brevo.com/font/Roboto/Latin/normal/normal/7529907e9eaf8ebb5220c5f9850e3811.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/normal/normal/25c678feafdc175a70922a116c9be3e7.woff) format("woff")
-    }
-
-    @font-face {
-      font-display: fallback;
-      font-family: Figtree;
-      font-weight: 600;
-      src: url(https://assets.brevo.com/font/Roboto/Latin/medium/normal/6e9caeeafb1f3491be3e32744bc30440.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/medium/normal/71501f0d8d5aa95960f6475d5487d4c2.woff) format("woff")
-    }
-
-    @font-face {
-      font-display: fallback;
-      font-family: Figtree;
-      font-weight: 700;
-      src: url(https://assets.brevo.com/font/Roboto/Latin/bold/normal/3ef7cf158f310cf752d5ad08cd0e7e60.woff2) format("woff2"), url(https://assets.brevo.com/font/Roboto/Latin/bold/normal/ece3a1d82f18b60bcce0211725c476aa.woff) format("woff")
-    }
-
-    #sib-container input:-ms-input-placeholder {
-      text-align: left;
-      font-family: Figtree, sans-serif;
-      color: #9bb2bd;
-    }
-
-    #sib-container input::placeholder {
-      text-align: left;
-      font-family: Figtree, sans-serif;
-      color: #9bb2bd;
-    }
-
-    #sib-container textarea::placeholder {
-      text-align: left;
-      font-family: Figtree, sans-serif;
-      color: #9bb2bd;
-    }
-
-    #sib-container a {
-      text-decoration: underline;
-      color: #2BB2FC;
-    }
-  </style>
-<link href="https://sibforms.com/forms/end-form/build/sib-styles.css" rel="stylesheet"/>
-<!--  END - We recommend to place the above code in head tag of your website html -->
-<!-- START - We recommend to place the below code where you want the form in your website html  -->
-<div class="sib-form" style="text-align: center;
-           background-color: transparent;                                 ">
-<div class="sib-form-container" id="sib-form-container">
-<div class="sib-form-message-panel" id="error-message" style="font-size:16px; text-align:left; font-family:Figtree, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;max-width:540px;">
-<div class="sib-form-message-panel__text sib-form-message-panel__text--center">
-<svg class="sib-icon sib-notification__icon" viewbox="0 0 512 512">
-<path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z"></path>
-</svg>
-<span class="sib-form-message-panel__inner-text">
-                            Your subscription could not be validated.
-                        </span>
 </div>
-</div>
-<div></div>
-<div class="sib-form-message-panel" id="success-message" style="font-size:16px; text-align:left; font-family:Figtree, sans-serif; color:#085229; background-color:#e7faf0; border-radius:3px; border-color:#13ce66;max-width:540px;">
-<div class="sib-form-message-panel__text sib-form-message-panel__text--center">
-<svg class="sib-icon sib-notification__icon" viewbox="0 0 512 512">
-<path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z"></path>
-</svg>
-<span class="sib-form-message-panel__inner-text">
-                            You have subscribed successfully.
-                        </span>
-</div>
-</div>
-<div></div>
-<div class="sib-container--large sib-container--vertical" id="sib-container" style="text-align:center; background-color:rgba(255,255,255,1); max-width:540px; border-radius:24px; border-width:1px; border-color:#C0CCD9; border-style:solid; direction:ltr">
-<form action="https://ec3bef76.sibforms.com/serve/MUIFAC-44qAlxzASbiA_fGejm7QyzA0cNzYNstzvkHj7FEFowq9XYICgmTeeLnUgWR-3cCbsovAZUiwm9iM5OwDSr1YJ172YN90f51NA39KXqeESyo1YctB1RA2-qVXPjPMUAs_G-aXObYmetoYqc6WGM0RR4mfuff1exbPeddpUWC3rNuko1otaQ7TKdDH7F6BGkMPOd_10bBrf" data-type="subscription" id="sib-form" method="POST">
-<div style="padding: 8px 0;">
-<div class="sib-form-block" style="font-size:32px; text-align:left; font-weight:700; font-family:Figtree, sans-serif; color:#33617a; background-color:transparent; text-align:left">
-<p>Newsletter</p>
-</div>
-</div>
-<div style="padding: 8px 0;">
-<div class="sib-form-block" style="font-size:16px; text-align:left; font-family:Figtree, sans-serif; color:#33617a; background-color:transparent; text-align:left">
-<div class="sib-text-form-block">
-<p>Subscribe to our newsletter and stay up to date.</p>
-</div>
-</div>
-</div>
-<div style="padding: 8px 0;">
-<div class="sib-input sib-form-block">
-<div class="form__entry entry_block">
-<div class="form__label-row">
-<label class="entry__label" data-required="*" for="EMAIL" style="font-weight: 700; text-align:left; font-size:16px; text-align:left; font-weight:700; font-family:Figtree, sans-serif; color:#33617a;">Enter your email to subscribe</label>
-<div class="entry__field">
-<input autocomplete="off" class="input" data-required="true" id="EMAIL" name="EMAIL" placeholder="EMAIL" required="" style="border-radius:0.7rem;" type="text">
-</input></div>
-</div>
-<label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:Figtree, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;">
-</label>
-<label class="entry__specification" style="font-size:12px; text-align:left; font-family:Figtree, sans-serif; color:#33617a; text-align:left">
-                  Please provide your email to subscribe. For example: abc@xyz.com
-                </label>
-</div>
-</div>
-</div>
-<div style="padding: 8px 0;">
-<div class="sib-optin sib-form-block">
-<div class="form__entry entry_mcq">
-<div class="form__label-row">
-<div class="entry__choice" style="">
-<label>
-<input class="input_replaced" id="OPT_IN" name="OPT_IN" type="checkbox" value="1">
-<span class="checkbox checkbox_tick_positive" style="margin-left:"></span><span style="font-size:14px; text-align:left; font-family:Figtree, sans-serif; color:#3C4858; background-color:transparent;"><p>I agree to receive newsletters under the appropriate terms and conditions.</p><p><br/></p></span> </input></label>
-</div>
-</div>
-<label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; font-family:Figtree, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;">
-</label>
-<label class="entry__specification" style="font-size:12px; text-align:left; font-family:Figtree, sans-serif; color:#33617a; text-align:left">
-                  You can unsubscribe at any time using the link in our newsletter.
-                </label>
-</div>
-</div>
-</div>
-<div style="padding: 8px 0;">
-<div class="sib-form-block" style="text-align: left">
-<button class="sib-form-block__button sib-form-block__button-with-loader" form="sib-form" style="font-size:16px; text-align:left; font-weight:700; font-family:Figtree, sans-serif; color:#FFFFFF; background-color:#33617a; border-radius:20px; border-width:0px;" type="submit">
-<svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewbox="0 0 512 512">
-<path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"></path>
-</svg>
-                SUBSCRIBE
-              </button>
-</div>
-</div>
-<input class="input--hidden" name="email_address_check" type="text" value=""/>
-<input name="locale" type="hidden" value="en"/>
-</form>
-</div>
-</div>
-</div>
-<!-- END - We recommend to place the above code where you want the form in your website html  -->
-<!-- START - We recommend to place the below code in footer or bottom of your website html  -->
-<script>
-    window.REQUIRED_CODE_ERROR_MESSAGE = 'Select a country code';
-    window.LOCALE = 'en';
-    window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "The information provided is invalid. Please check the field format and try again.";
-
-    window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank.";
-
-    window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please check the field format and try again.";
-
-    window.translation = {
-      common: {
-        selectedList: '{quantity} selected list',
-        selectedLists: '{quantity} selected lists'
-      }
-    };
-
-    var AUTOHIDE = Boolean(0);
-  </script>
-<script defer="" src="https://sibforms.com/forms/end-form/build/main.js"></script>
-<!-- END - We recommend to place the above code in footer or bottom of your website html  -->
-<!-- End Brevo Form -->
-</div><noscript>
-<img alt="" height="1" src="https://px.ads.linkedin.com/collect/?pid=8191113&amp;fmt=gif" style="display:none;" width="1">
-</img></noscript>
 
 <?php get_footer(); ?>
